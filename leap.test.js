cc0fe09bdbf998bbfbe9b5c10dd142f7e388c6cc
Ben Pardo <benpardo@bens-mbp-4.lan>
2017-08-24 15:05:26 -0600

    verifies every 100 and 400 year leaps

--- leap.test.js
+++ leap.test.js
@@ -2,8 +2,7 @@ const {store} = require("./leap");
 const {changeYear} = require("./actions");
 
 test("a year that is not a leap year", function () {
-    store.dispatch(changeYear(2015));
-
+    store.dispatch(changeYear(2015)); 
     expect(store.getState().isLeap).toBe(false);
 });
 
@@ -21,6 +20,19 @@ test("a year that is not a leap year", function () {
 
 test("another year that is a leap year", function () {
     store.dispatch(changeYear(2012));
+
+    expect(store.getState().isLeap).toBe(true);
+
+});
+
+test("every one hundred years we skip a leap year", function () {
+    store.dispatch(changeYear(1900));
+
+    expect(store.getState().isLeap).toBe(false);
+});
+
+test("every four hundred years we bring the leap year back", function () {
+    store.dispatch(changeYear(2000));
 
     expect(store.getState().isLeap).toBe(true);
 
--- reducers.js
+++ reducers.js
@@ -1,5 +1,15 @@
 module.exports.checkLeapYear = function (state = {isLeap: false}, action) {
     if(action.year % 4 === 0) {
+        if(action.year % 100 === 0) {
+            if(action.year % 400 === 0) {
+                return {
+                    isLeap: true
+                }
+            }
+            return {
+                isLeap: false
+            }
+        }
         return {
             isLeap: true
         }
